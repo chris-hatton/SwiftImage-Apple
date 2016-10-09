eafbb0e429568fa0a77054a26144f6b11e25bbff
Chris Hatton <christopherhattonuk@gmail.com>
2016-10-09 17:22:21 +1100

    Initial Commit

---
+++ SwiftImage-Apple/SwiftImage-Apple.h
@@ -0,0 +1,19 @@
+//
+//  SwiftImage-Apple.h
+//  SwiftImage-Apple
+//
+//  Created by Christopher Hatton on 09/10/2016.
+//  Copyright © 2016 Chris Hatton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftImage-Apple.
+FOUNDATION_EXPORT double SwiftImage_AppleVersionNumber;
+
+//! Project version string for SwiftImage-Apple.
+FOUNDATION_EXPORT const unsigned char SwiftImage_AppleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftImage_Apple/PublicHeader.h>
+
+
