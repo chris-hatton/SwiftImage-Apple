fe053001dec81e621b7a9f117ac1e4a4d933a326
Chris Hatton <christopherhattonuk@gmail.com>
2017-03-15 20:58:28 +1100

    Sync

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 //
 //  Package.swift
-//  SwiftImage-Apple
+//  SwiftImage-iOS
 //
 //  Created by Christopher Hatton on 09/10/2016.
 //  Copyright © 2016 Chris Hatton. All rights reserved.
@@ -9,7 +9,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftImage-Apple"
+    name: "SwiftImage-iOS”
     targets: [],
     dependencies: [ .Package(url: “../SwiftImage“, majorVersion: 1) ]
 )
